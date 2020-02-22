94349d91bce32f75dea6a6afb74e5ce30f91a019
bluewelkin <denggx@126.com>
2020-02-22 21:20:45 +0800

    ribbon 负载均衡完成

--- src/main/java/com/itmuch/contentcenter/ContentcenterApplication.java
+++ src/main/java/com/itmuch/contentcenter/ContentcenterApplication.java
@@ -2,6 +2,7 @@ package com.itmuch.contentcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -13,6 +14,7 @@ public class ContentcenterApplication {
         SpringApplication.run(ContentcenterApplication.class, args);
     }
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
        return new RestTemplate();
     }
--- src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
+++ src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
@@ -5,6 +5,7 @@ import com.itmuch.contentcenter.domain.dto.content.ShareDTO;
 import com.itmuch.contentcenter.domain.dto.user.UserDTO;
 import com.itmuch.contentcenter.domain.entity.content.Share;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class ShareService {
     private final ShareMapper shareMapper;
     private final RestTemplate restTemplate;
@@ -19,11 +21,14 @@ public class ShareService {
     public ShareDTO findById(Integer id){
         Share share = this.shareMapper.selectByPrimaryKey(id);
         Integer userId=share.getUserId();
+        String targerUrl = "Http://usercenterprovider/users/{id}";
 
         UserDTO userDTO= this.restTemplate.getForObject(
-                "Http://localhost:8080/users/{id}",
+                targerUrl,
                 UserDTO.class,userId
         );
+        log.info(targerUrl);
+
         ShareDTO shaeDTO =new ShareDTO();
         BeanUtils.copyProperties(share,shaeDTO);
         shaeDTO.setWxNickName(userDTO.getWxNickname());
