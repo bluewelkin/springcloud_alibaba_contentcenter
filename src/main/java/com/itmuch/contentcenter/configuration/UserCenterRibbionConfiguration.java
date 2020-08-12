b1ab846d2ac592a40e80b8994b0f6845a1c4a77d
bluewelkin <denggx@126.com>
2020-08-12 22:20:26 +0800

    6-8 属性配置随机负载均衡代替java代码

--- src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbionConfiguration.java
+++ src/main/java/com/itmuch/contentcenter/configuration/UserCenterRibbionConfiguration.java
@@ -4,8 +4,8 @@ package com.itmuch.contentcenter.configuration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 import ribbionconfiguration.RibbonConfiguration;
-
-@Configuration
-@RibbonClient(name = "usercenterprovider",configuration = RibbonConfiguration.class)
+//
+//@Configuration
+//@RibbonClient(name = "usercenterprovider",configuration = RibbonConfiguration.class)
 public class UserCenterRibbionConfiguration {
 }
