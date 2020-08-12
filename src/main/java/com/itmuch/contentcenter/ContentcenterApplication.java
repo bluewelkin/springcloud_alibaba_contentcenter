be2948ef65c5fb6b23bdff6ad0e33c01ca6cfa1c
bluewelkin <denggx@126.com>
2020-08-12 20:14:23 +0800

    nacos version update

--- src/main/java/com/itmuch/contentcenter/ContentcenterApplication.java
+++ src/main/java/com/itmuch/contentcenter/ContentcenterApplication.java
@@ -2,6 +2,7 @@ package com.itmuch.contentcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.itmuch")
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ContentcenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentcenterApplication.class, args);
